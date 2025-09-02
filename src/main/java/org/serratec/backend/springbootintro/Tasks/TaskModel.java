61bda529ce2d49b322b614abc4d84e14bf36a023
tvlisboa <tvlisboa@hotmail.com>
2025-09-02 11:33:23 -0300

    troca de tipo de identificacao no banco de dados

--- src/main/java/org/serratec/backend/springbootintro/Tasks/TaskModel.java
+++ src/main/java/org/serratec/backend/springbootintro/Tasks/TaskModel.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class TaskModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String descricao;
 
